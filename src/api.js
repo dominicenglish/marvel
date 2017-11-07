c18f295f14daa815b5c55516d019641285405ff2
Dominic English <dominic.b.english@gmail.com>
2017-11-07 12:38:38 +1000

    Move api key into config file

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import * as queryString from 'query-string';
 import { normalize, schema, arrayOf } from 'normalizr';
+import { apiKey } from './config.js';
 
 const endpoint = 'https://gateway.marvel.com:443/v1/public';
-const apiKey = '4a774e5516f38b3e92843d5b4babcd45';
 
 const results = new schema.Entity('results');
 const resultsSchema = [ results ];
